f7c493600c4eced83c1a28efab38bc7ddadcf140
Alina Nosovets <alinanosovets6849@gmail.com>
2024-03-05 22:32:03 +0700

    feat: implement methods override for user create, update

--- src/repositories/user.repository.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { BaseRepository } from './in-memory-db/base.repository';
-import { User } from '../models/user.entity';
-
-@Injectable()
-export class UserRepository extends BaseRepository<User> {}
