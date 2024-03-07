f2886b244cbcf37928699e7688962ecdd1b813a6
Alina Nosovets <alinanosovets6849@gmail.com>
2024-03-07 12:09:50 +0700

    feat: integrate swagger with app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as fs from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +13,24 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const options = new DocumentBuilder()
+    .setTitle('Example API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  if (!fs.existsSync('./doc')) {
+    fs.mkdirSync('./doc');
+  }
+
+  fs.writeFileSync(
+    './doc/openapi-spec.json',
+    JSON.stringify(document, null, 2),
+  );
+
   await app.listen(4000);
 }
 bootstrap();
