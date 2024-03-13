d3a0be15f8ecdc33d90bfd606a49c7303764f382
Alina Nosovets <alinanosovets6849@gmail.com>
2024-03-13 21:51:33 +0700

    refactor: update artist service to use postgres db

--- src/artist/artist.module.ts
+++ src/artist/artist.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ArtistService } from './artist.service';
 import { ArtistController } from './artist.controller';
-import { ArtistRepository } from './artist.repository';
 import { TrackModule } from 'src/track/track.module';
 import { AlbumModule } from 'src/album/album.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Artist } from 'src/entities/artist.entity';
 
 @Module({
-  imports: [TrackModule, AlbumModule],
+  imports: [TypeOrmModule.forFeature([Artist]), TrackModule, AlbumModule],
   controllers: [ArtistController],
-  providers: [ArtistService, ArtistRepository],
+  providers: [ArtistService],
   exports: [ArtistService],
 })
 export class ArtistModule {}
--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,43 +1,66 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { ArtistRepository } from './artist.repository';
-import { Artist } from '../models/artist';
 import { AlbumService } from 'src/album/album.service';
 import { TrackService } from 'src/track/track.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Artist } from 'src/entities/artist.entity';
+import { Repository } from 'typeorm';
+import { CreateArtistDto } from './dto/create-artist.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class ArtistService {
   constructor(
-    private readonly artistRepository: ArtistRepository,
+    @InjectRepository(Artist)
+    private readonly artistRepository: Repository<Artist>,
     private readonly trackService: TrackService,
     private readonly albumService: AlbumService,
   ) {}
 
-  async createArtist(artist: Omit<Artist, 'id'>): Promise<Artist> {
-    return this.artistRepository.create(artist);
+  async createArtist(createArtistDto: CreateArtistDto): Promise<Artist> {
+    const newArtist = {
+      id: uuidv4(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+    const artist = this.artistRepository.create({
+      ...newArtist,
+    });
+
+    return await this.artistRepository.save(artist);
   }
 
   async getArtistById(id: string): Promise<Artist | null> {
-    return this.artistRepository.findById(id);
+    const artist = await this.artistRepository.findOneBy({ id });
+
+    if (!artist) {
+      return null;
+    }
+    return artist;
   }
 
   async getAllArtists(): Promise<Artist[]> {
-    return this.artistRepository.findAll();
+    return await this.artistRepository.find();
   }
 
   async updateArtist(
     id: string,
     artistUpdates: Partial<Artist>,
   ): Promise<Artist> {
-    return this.artistRepository.update(id, { id, ...artistUpdates });
+    const artist = await this.artistRepository.findOneBy({ id });
+
+    if (!artist) {
+      return null;
+    }
+    await this.artistRepository.update(id, artistUpdates);
+
+    return await this.getArtistById(id);
   }
 
   async deleteArtist(id: string): Promise<void> {
-    const artist = await this.artistRepository.findById(id);
-    if (!artist) {
-      throw new NotFoundException(`Artist with ID ${id} not found`);
+    const result = await this.artistRepository.delete(id);
+    if (result.affected === 0) {
+      throw new NotFoundException(`Album with ID ${id} not found`);
     }
-
-    await this.artistRepository.delete(id);
 
     await this.trackService.setArtistIdToNullForTrack(id);
     await this.albumService.setArtistIdToNullForAlbum(id);
