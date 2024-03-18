a7b16f01c1deb5eb6136bafb6a9333d4e7d2ea48
Alina Nosovets <alinanosovets6849@gmail.com>
2024-03-18 22:40:15 +0700

    refactor: delete redundant code from previous implementation with in-memory db

--- src/artist/artist.repository.ts
+++
@@ -1,4 +0,0 @@
-import { BaseRepository } from '../repositories/in-memory-db/base.repository';
-import { Artist } from '../models/artist';
-
-export class ArtistRepository extends BaseRepository<Artist> {}
--- src/repositories/abstract-crud-repository.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface AbstractCrudRepository<T, ID> {
-  create(item: T): Promise<T>;
-  findById(id: ID): Promise<T | null>;
-  findAll(): Promise<T[]>;
-  update(id: ID, item: T): Promise<T>;
-  delete(id: ID): Promise<void>;
-}
